4057a9b29bfb95ca5b19435c68b4a711647cee7a
stefvanesch <stefvanesch@users.noreply.github.com>
2020-10-21 12:36:52 +0000

    Fix styling

--- src/Console/Commands/InstallCommand.php
+++ src/Console/Commands/InstallCommand.php
@@ -3,7 +3,6 @@
 namespace Marshmallow\Translatable\Console\Commands;
 
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Artisan;
 
 class InstallCommand extends Command
