de051023f0b5e3825adfd549e3bd08eb1b006dbe
Stef van Esch <stef@marshmallow.dev>
2020-10-21 18:21:22 +0200

    Added presets

---
+++ resources/presets/en.php
@@ -0,0 +1,573 @@
+<?php
+
+return [
+    'locale' => 'en',
+    'name' => 'English',
+    'preset' => [
+        0 => [
+            'group' => 'auth',
+            'key' => 'failed',
+            'value' => 'These credentials do not match our records.',
+        ],
+        1 => [
+            'group' => 'auth',
+            'key' => 'throttle',
+            'value' => 'Too many login attempts. Please try again in :seconds seconds.',
+        ],
+        2 => [
+            'group' => 'pagination',
+            'key' => 'previous',
+            'value' => '&laquo; Previous',
+        ],
+        3 => [
+            'group' => 'pagination',
+            'key' => 'next',
+            'value' => 'Next &raquo;',
+        ],
+        4 => [
+            'group' => 'passwords',
+            'key' => 'reset',
+            'value' => 'Your password has been reset!',
+        ],
+        5 => [
+            'group' => 'passwords',
+            'key' => 'sent',
+            'value' => 'We have emailed your password reset link!',
+        ],
+        6 => [
+            'group' => 'passwords',
+            'key' => 'throttled',
+            'value' => 'Please wait before retrying.',
+        ],
+        7 => [
+            'group' => 'passwords',
+            'key' => 'token',
+            'value' => 'This password reset token is invalid.',
+        ],
+        8 => [
+            'group' => 'passwords',
+            'key' => 'user',
+            'value' => 'We can\'t find a user with that email address.',
+        ],
+        9 => [
+            'group' => 'validation',
+            'key' => 'accepted',
+            'value' => 'The :attribute must be accepted.',
+        ],
+        10 => [
+            'group' => 'validation',
+            'key' => 'active_url',
+            'value' => 'The :attribute is not a valid URL.',
+        ],
+        11 => [
+            'group' => 'validation',
+            'key' => 'after',
+            'value' => 'The :attribute must be a date after :date.',
+        ],
+        12 => [
+            'group' => 'validation',
+            'key' => 'after_or_equal',
+            'value' => 'The :attribute must be a date after or equal to :date.',
+        ],
+        13 => [
+            'group' => 'validation',
+            'key' => 'alpha',
+            'value' => 'The :attribute may only contain letters.',
+        ],
+        14 => [
+            'group' => 'validation',
+            'key' => 'alpha_dash',
+            'value' => 'The :attribute may only contain letters, numbers, dashes and underscores.',
+        ],
+        15 => [
+            'group' => 'validation',
+            'key' => 'alpha_num',
+            'value' => 'The :attribute may only contain letters and numbers.',
+        ],
+        16 => [
+            'group' => 'validation',
+            'key' => 'array',
+            'value' => 'The :attribute must be an array.',
+        ],
+        17 => [
+            'group' => 'validation',
+            'key' => 'before',
+            'value' => 'The :attribute must be a date before :date.',
+        ],
+        18 => [
+            'group' => 'validation',
+            'key' => 'before_or_equal',
+            'value' => 'The :attribute must be a date before or equal to :date.',
+        ],
+        19 => [
+            'group' => 'validation',
+            'key' => 'between.numeric',
+            'value' => 'The :attribute must be between :min and :max.',
+        ],
+        20 => [
+            'group' => 'validation',
+            'key' => 'between.file',
+            'value' => 'The :attribute must be between :min and :max kilobytes.',
+        ],
+        21 => [
+            'group' => 'validation',
+            'key' => 'between.string',
+            'value' => 'The :attribute must be between :min and :max characters.',
+        ],
+        22 => [
+            'group' => 'validation',
+            'key' => 'between.array',
+            'value' => 'The :attribute must have between :min and :max items.',
+        ],
+        23 => [
+            'group' => 'validation',
+            'key' => 'boolean',
+            'value' => 'The :attribute field must be true or false.',
+        ],
+        24 => [
+            'group' => 'validation',
+            'key' => 'confirmed',
+            'value' => 'The :attribute confirmation does not match.',
+        ],
+        25 => [
+            'group' => 'validation',
+            'key' => 'date',
+            'value' => 'The :attribute is not a valid date.',
+        ],
+        26 => [
+            'group' => 'validation',
+            'key' => 'date_equals',
+            'value' => 'The :attribute must be a date equal to :date.',
+        ],
+        27 => [
+            'group' => 'validation',
+            'key' => 'date_format',
+            'value' => 'The :attribute does not match the format :format.',
+        ],
+        28 => [
+            'group' => 'validation',
+            'key' => 'different',
+            'value' => 'The :attribute and :other must be different.',
+        ],
+        29 => [
+            'group' => 'validation',
+            'key' => 'digits',
+            'value' => 'The :attribute must be :digits digits.',
+        ],
+        30 => [
+            'group' => 'validation',
+            'key' => 'digits_between',
+            'value' => 'The :attribute must be between :min and :max digits.',
+        ],
+        31 => [
+            'group' => 'validation',
+            'key' => 'dimensions',
+            'value' => 'The :attribute has invalid image dimensions.',
+        ],
+        32 => [
+            'group' => 'validation',
+            'key' => 'distinct',
+            'value' => 'The :attribute field has a duplicate value.',
+        ],
+        33 => [
+            'group' => 'validation',
+            'key' => 'email',
+            'value' => 'The :attribute must be a valid email address.',
+        ],
+        34 => [
+            'group' => 'validation',
+            'key' => 'ends_with',
+            'value' => 'The :attribute must end with one of the following: :values.',
+        ],
+        35 => [
+            'group' => 'validation',
+            'key' => 'exists',
+            'value' => 'The selected :attribute is invalid.',
+        ],
+        36 => [
+            'group' => 'validation',
+            'key' => 'file',
+            'value' => 'The :attribute must be a file.',
+        ],
+        37 => [
+            'group' => 'validation',
+            'key' => 'filled',
+            'value' => 'The :attribute field must have a value.',
+        ],
+        38 => [
+            'group' => 'validation',
+            'key' => 'gt.numeric',
+            'value' => 'The :attribute must be greater than :value.',
+        ],
+        39 => [
+            'group' => 'validation',
+            'key' => 'gt.file',
+            'value' => 'The :attribute must be greater than :value kilobytes.',
+        ],
+        40 => [
+            'group' => 'validation',
+            'key' => 'gt.string',
+            'value' => 'The :attribute must be greater than :value characters.',
+        ],
+        41 => [
+            'group' => 'validation',
+            'key' => 'gt.array',
+            'value' => 'The :attribute must have more than :value items.',
+        ],
+        42 => [
+            'group' => 'validation',
+            'key' => 'gte.numeric',
+            'value' => 'The :attribute must be greater than or equal :value.',
+        ],
+        43 => [
+            'group' => 'validation',
+            'key' => 'gte.file',
+            'value' => 'The :attribute must be greater than or equal :value kilobytes.',
+        ],
+        44 => [
+            'group' => 'validation',
+            'key' => 'gte.string',
+            'value' => 'The :attribute must be greater than or equal :value characters.',
+        ],
+        45 => [
+            'group' => 'validation',
+            'key' => 'gte.array',
+            'value' => 'The :attribute must have :value items or more.',
+        ],
+        46 => [
+            'group' => 'validation',
+            'key' => 'image',
+            'value' => 'The :attribute must be an image.',
+        ],
+        47 => [
+            'group' => 'validation',
+            'key' => 'in',
+            'value' => 'The selected :attribute is invalid.',
+        ],
+        48 => [
+            'group' => 'validation',
+            'key' => 'in_array',
+            'value' => 'The :attribute field does not exist in :other.',
+        ],
+        49 => [
+            'group' => 'validation',
+            'key' => 'integer',
+            'value' => 'The :attribute must be an integer.',
+        ],
+        50 => [
+            'group' => 'validation',
+            'key' => 'ip',
+            'value' => 'The :attribute must be a valid IP address.',
+        ],
+        51 => [
+            'group' => 'validation',
+            'key' => 'ipv4',
+            'value' => 'The :attribute must be a valid IPv4 address.',
+        ],
+        52 => [
+            'group' => 'validation',
+            'key' => 'ipv6',
+            'value' => 'The :attribute must be a valid IPv6 address.',
+        ],
+        53 => [
+            'group' => 'validation',
+            'key' => 'json',
+            'value' => 'The :attribute must be a valid JSON string.',
+        ],
+        54 => [
+            'group' => 'validation',
+            'key' => 'lt.numeric',
+            'value' => 'The :attribute must be less than :value.',
+        ],
+        55 => [
+            'group' => 'validation',
+            'key' => 'lt.file',
+            'value' => 'The :attribute must be less than :value kilobytes.',
+        ],
+        56 => [
+            'group' => 'validation',
+            'key' => 'lt.string',
+            'value' => 'The :attribute must be less than :value characters.',
+        ],
+        57 => [
+            'group' => 'validation',
+            'key' => 'lt.array',
+            'value' => 'The :attribute must have less than :value items.',
+        ],
+        58 => [
+            'group' => 'validation',
+            'key' => 'lte.numeric',
+            'value' => 'The :attribute must be less than or equal :value.',
+        ],
+        59 => [
+            'group' => 'validation',
+            'key' => 'lte.file',
+            'value' => 'The :attribute must be less than or equal :value kilobytes.',
+        ],
+        60 => [
+            'group' => 'validation',
+            'key' => 'lte.string',
+            'value' => 'The :attribute must be less than or equal :value characters.',
+        ],
+        61 => [
+            'group' => 'validation',
+            'key' => 'lte.array',
+            'value' => 'The :attribute must not have more than :value items.',
+        ],
+        62 => [
+            'group' => 'validation',
+            'key' => 'max.numeric',
+            'value' => 'The :attribute may not be greater than :max.',
+        ],
+        63 => [
+            'group' => 'validation',
+            'key' => 'max.file',
+            'value' => 'The :attribute may not be greater than :max kilobytes.',
+        ],
+        64 => [
+            'group' => 'validation',
+            'key' => 'max.string',
+            'value' => 'The :attribute may not be greater than :max characters.',
+        ],
+        65 => [
+            'group' => 'validation',
+            'key' => 'max.array',
+            'value' => 'The :attribute may not have more than :max items.',
+        ],
+        66 => [
+            'group' => 'validation',
+            'key' => 'mimes',
+            'value' => 'The :attribute must be a file of type: :values.',
+        ],
+        67 => [
+            'group' => 'validation',
+            'key' => 'mimetypes',
+            'value' => 'The :attribute must be a file of type: :values.',
+        ],
+        68 => [
+            'group' => 'validation',
+            'key' => 'min.numeric',
+            'value' => 'The :attribute must be at least :min.',
+        ],
+        69 => [
+            'group' => 'validation',
+            'key' => 'min.file',
+            'value' => 'The :attribute must be at least :min kilobytes.',
+        ],
+        70 => [
+            'group' => 'validation',
+            'key' => 'min.string',
+            'value' => 'The :attribute must be at least :min characters.',
+        ],
+        71 => [
+            'group' => 'validation',
+            'key' => 'min.array',
+            'value' => 'The :attribute must have at least :min items.',
+        ],
+        72 => [
+            'group' => 'validation',
+            'key' => 'not_in',
+            'value' => 'The selected :attribute is invalid.',
+        ],
+        73 => [
+            'group' => 'validation',
+            'key' => 'not_regex',
+            'value' => 'The :attribute format is invalid.',
+        ],
+        74 => [
+            'group' => 'validation',
+            'key' => 'numeric',
+            'value' => 'The :attribute must be a number.',
+        ],
+        75 => [
+            'group' => 'validation',
+            'key' => 'password',
+            'value' => 'The password is incorrect.',
+        ],
+        76 => [
+            'group' => 'validation',
+            'key' => 'present',
+            'value' => 'The :attribute field must be present.',
+        ],
+        77 => [
+            'group' => 'validation',
+            'key' => 'regex',
+            'value' => 'The :attribute format is invalid.',
+        ],
+        78 => [
+            'group' => 'validation',
+            'key' => 'required',
+            'value' => 'The :attribute field is required.',
+        ],
+        79 => [
+            'group' => 'validation',
+            'key' => 'required_if',
+            'value' => 'The :attribute field is required when :other is :value.',
+        ],
+        80 => [
+            'group' => 'validation',
+            'key' => 'required_unless',
+            'value' => 'The :attribute field is required unless :other is in :values.',
+        ],
+        81 => [
+            'group' => 'validation',
+            'key' => 'required_with',
+            'value' => 'The :attribute field is required when :values is present.',
+        ],
+        82 => [
+            'group' => 'validation',
+            'key' => 'required_with_all',
+            'value' => 'The :attribute field is required when :values are present.',
+        ],
+        83 => [
+            'group' => 'validation',
+            'key' => 'required_without',
+            'value' => 'The :attribute field is required when :values is not present.',
+        ],
+        84 => [
+            'group' => 'validation',
+            'key' => 'required_without_all',
+            'value' => 'The :attribute field is required when none of :values are present.',
+        ],
+        85 => [
+            'group' => 'validation',
+            'key' => 'same',
+            'value' => 'The :attribute and :other must match.',
+        ],
+        86 => [
+            'group' => 'validation',
+            'key' => 'size.numeric',
+            'value' => 'The :attribute must be :size.',
+        ],
+        87 => [
+            'group' => 'validation',
+            'key' => 'size.file',
+            'value' => 'The :attribute must be :size kilobytes.',
+        ],
+        88 => [
+            'group' => 'validation',
+            'key' => 'size.string',
+            'value' => 'The :attribute must be :size characters.',
+        ],
+        89 => [
+            'group' => 'validation',
+            'key' => 'size.array',
+            'value' => 'The :attribute must contain :size items.',
+        ],
+        90 => [
+            'group' => 'validation',
+            'key' => 'starts_with',
+            'value' => 'The :attribute must start with one of the following: :values.',
+        ],
+        91 => [
+            'group' => 'validation',
+            'key' => 'string',
+            'value' => 'The :attribute must be a string.',
+        ],
+        92 => [
+            'group' => 'validation',
+            'key' => 'timezone',
+            'value' => 'The :attribute must be a valid zone.',
+        ],
+        93 => [
+            'group' => 'validation',
+            'key' => 'unique',
+            'value' => 'The :attribute has already been taken.',
+        ],
+        94 => [
+            'group' => 'validation',
+            'key' => 'uploaded',
+            'value' => 'The :attribute failed to upload.',
+        ],
+        95 => [
+            'group' => 'validation',
+            'key' => 'url',
+            'value' => 'The :attribute format is invalid.',
+        ],
+        96 => [
+            'group' => 'validation',
+            'key' => 'uuid',
+            'value' => 'The :attribute must be a valid UUID.',
+        ],
+        97 => [
+            'group' => 'validation',
+            'key' => 'custom.attribute-name.rule-name',
+            'value' => 'custom-message',
+        ],
+        98 => [
+            'group' => 'validation',
+            'key' => 'attached',
+            'value' => 'This :attribute is already attached.',
+        ],
+        99 => [
+            'group' => 'validation',
+            'key' => 'relatable',
+            'value' => 'This :attribute may not be associated with this resource.',
+        ],
+        100 => [
+            'group' => 'single',
+            'key' => 'Email Address',
+            'value' => 'Email Address',
+        ],
+        101 => [
+            'group' => 'single',
+            'key' => 'Login',
+            'value' => 'Login',
+        ],
+        102 => [
+            'group' => 'single',
+            'key' => 'Logout',
+            'value' => 'Logout',
+        ],
+        103 => [
+            'group' => 'single',
+            'key' => 'Password',
+            'value' => 'Password',
+        ],
+        104 => [
+            'group' => 'single',
+            'key' => 'Remember Me',
+            'value' => 'Remember Me',
+        ],
+        105 => [
+            'group' => 'single',
+            'key' => 'Press / to search',
+            'value' => 'Press / to search',
+        ],
+        106 => [
+            'group' => 'single',
+            'key' => 'Nova User',
+            'value' => 'Nova User',
+        ],
+        107 => [
+            'group' => 'single',
+            'key' => 'Key',
+            'value' => 'Key',
+        ],
+        108 => [
+            'group' => 'single',
+            'key' => 'Value',
+            'value' => 'Value',
+        ],
+        109 => [
+            'group' => 'single',
+            'key' => 'Create :resource',
+            'value' => 'Create :resource',
+        ],
+        110 => [
+            'group' => 'single',
+            'key' => 'Do you really want to leave? You have unsaved changes.',
+            'value' => 'Do you really want to leave? You have unsaved changes.',
+        ],
+        111 => [
+            'group' => 'single',
+            'key' => 'ID',
+            'value' => 'ID',
+        ],
+        112 => [
+            'group' => 'single',
+            'key' => 'Welcome!',
+            'value' => 'Welkom terug!',
+        ],
+    ],
+];
