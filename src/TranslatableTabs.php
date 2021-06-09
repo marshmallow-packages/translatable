3b2ad3fe2c490a5c6343e7bec682946987a545e9
Stef van Esch <stef@marshmallow.dev>
2021-06-09 09:42:21 +0200

    Fixed issue

--- src/TranslatableTabs.php
+++ src/TranslatableTabs.php
@@ -9,6 +9,10 @@ class TranslatableTabs
 {
     public function make(Resource $resource, string $name, array $tabs): Tabs
     {
+        if ($resource->weAreNotTranslating() || (request()->has('editMode') && 'create' == request()->editMode)) {
+            return new Tabs($name, $tabs);
+        }
+
         foreach ($tabs as $tab_name => $fields) {
             foreach ($fields as $key => $field) {
                 if (isset($field->attribute)) {
