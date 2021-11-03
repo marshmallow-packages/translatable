d0d49fbc53130755e4cb17b2f604b3220bf0f56a
Stef van Esch <stef@marshmallow.dev>
2021-11-03 10:35:22 +0100

    Scan files for field names for attribute translations

--- src/Scanner/Scanner.php
+++ src/Scanner/Scanner.php
@@ -19,12 +19,20 @@ class Scanner
         $this->translationMethods = $translationMethods;
     }
 
+    public function findTranslations()
+    {
+        $results = $this->findDefaultTranslations();
+        $results = $this->findValidationAttributes($results);
+
+        return $results;
+    }
+
     /**
      * Scan all the files in the provided $scanPath for translations.
      *
      * @return array
      */
-    public function findTranslations()
+    protected function findDefaultTranslations()
     {
         $results = ['single' => [], 'group' => []];
 
@@ -32,15 +40,15 @@ class Scanner
         // * Laravel Language Manager GUI from Mohamed Said (https://github.com/themsaid/laravel-langman-gui)
         // * Laravel 5 Translation Manager from Barry vd. Heuvel (https://github.com/barryvdh/laravel-translation-manager)
         $matchingPattern =
-            '[^\w]'. // Must not start with any alphanum or _
-            '(?<!->)'. // Must not start with ->
-            '('.implode('|', $this->translationMethods).')'. // Must start with one of the functions
-            "\(". // Match opening parentheses
-            "[\'\"]". // Match " or '
-            '('. // Start a new group to match:
-            '.+'. // Must start with group
-            ')'. // Close group
-            "[\'\"]". // Closing quote
+            '[^\w]' . // Must not start with any alphanum or _
+            '(?<!->)' . // Must not start with ->
+            '(' . implode('|', $this->translationMethods) . ')' . // Must start with one of the functions
+            "\(" . // Match opening parentheses
+            "[\'\"]" . // Match " or '
+            '(' . // Start a new group to match:
+            '.+' . // Must start with group
+            ')' . // Close group
+            "[\'\"]" . // Closing quote
             "[\),]";  // Close parentheses or new parameter
         foreach ($this->disk->allFiles($this->scanPaths) as $file) {
             if (preg_match_all("/$matchingPattern/siU", $file->getContents(), $matches)) {
@@ -52,6 +60,39 @@ class Scanner
                         continue;
                     } else {
                         $results['single']['single'][$key] = '';
+                    }
+                }
+            }
+        }
+
+        return $results;
+    }
+
+    /**
+     * Scan all the files in the provided $scanPath for fields names for validation attribute translations.
+     *
+     * @return array
+     */
+    protected function findValidationAttributes(array $results)
+    {
+        if (!array_key_exists('validation', $results['single'])) {
+            $results['single']['validation'] = [];
+        }
+
+        $scan_paths = [
+            base_path('resources/views')
+        ];
+
+        foreach ($this->disk->allFiles($scan_paths) as $file) {
+            if (preg_match_all("/(name|wire:model)=\"(.+?)\"/", $file->getContents(), $matches)) {
+                foreach ($matches[2] as $key) {
+                    if (preg_match("/(^[a-zA-Z0-9:_-]+([.][^\1)\ ]+)+$)/siU", $key, $arrayMatches)) {
+                        continue;
+                    } else {
+                        if ($key !== 'terms_accepted') {
+                            continue;
+                        }
+                        $results['single']['validation']["attributes.{$key}"] = '';
                     }
                 }
             }
